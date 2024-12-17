12d9779e2d310c1eb4085c38b21d5d078732a9f6
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2024-12-17 16:01:07 +0500

    Add files via upload

---
+++ break_conti.py
@@ -0,0 +1,70 @@
+# for i in range(12):
+#      if(i == 10):
+          
+#        break
+# print("3 *",i+1,"=",3*(i+1))
+# print("Now you are out of the loop")
+   
+    # tjis is like a do while loop 
+# i=0
+# while True:
+#    
+#     print(i)
+        #  i=i+1
+#     if i==20:
+#         break
+
+
+
+#Q print the table according to the user input
+# op=int(input("Enter number and get its table"))
+# i=1
+# while i<=10:
+#     print(op,"*",i,"= ",i*op)
+#     i =i+1
+
+
+
+# my_list=["Hello","Abdul samad ","jamali","how are you"]
+# i=1
+# length=len(my_list)-1
+# while i<=length:
+#     print(my_list[i])
+#     i=i+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+list1=(12,11,13,33,1,12,5,6)
+for i in list1:
+    if(i==33):
+        print("33 founded")
+    else:
+        print(i)
+       
+ 
+print("Program ended")
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   
