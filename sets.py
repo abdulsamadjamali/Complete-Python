0ff9a0f7a233edd37502056c391c3f00df2d8fb3
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2024-12-23 11:06:31 +0500

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,47 @@
+
+# my_sdata={"Abdul samad",21,True,32,21}
+# print(type(my_sdata))
+
+
+# for i in my_sdata:
+#     print(i)
+
+
+# Methods
+
+fruit1={"apple","Banana","Orange","Mango"}
+fruit2={"PineApple","Orange","Watermelon","Mango"}
+
+# print(fruit1.union(fruit2))
+
+# fruit1.update(fruit2)
+# print(fruit1)
+
+# fruit1.intersection_update(fruit2)
+# print(fruit1)
+
+# print(fruit1.symmetric_difference(fruit2))
+
+# print(fruit1.difference(fruit2))
+
+# print(fruit1.issuperset(fruit2))
+
+# print(fruit1.issubset(fruit2))
+
+# fruit1.remove("apple")
+# print(fruit1)
+
+# fruit1.add("strawberry")
+# print(fruit1)
+
+# fruit1.clear
+# print(fruit1)
+
+
+# fruit1.pop()
+# print(fruit1)
+
+
+
+
+
