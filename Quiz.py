85e624b702fa92bc7b603f848c8b1942c206cabf
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2024-12-31 14:09:20 +0500

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,76 @@
+
+# name="abdul samad jamali"
+# result=name.split(" ")[-1]
+# print(result)
+
+# num=0
+# sum=0
+# for num in range(1,11):
+#   sum+=num
+#   print(sum)
+
+
+
+# op=[1,2,3,4,5]
+# for i in op:
+#     print(op[i])
+
+# def total(a=int(input("Enter a number"))):
+#  for i in range(a):
+#     a+=i
+#     print(a)
+ 
+#  total(a)
+
+
+
+
+# a=int(input("Enter two numbers "))
+# b=int(input("Enter two numbers "))
+# def greater(a,b):
+#     if(a>b):
+#         print("A is grater than b ")
+#     elif(a<b):
+#         print("B is greater than A ")
+#     elif(a==b):
+#         print("ooohoo both number arre equla so there sum = :",a+b)
+    
+
+
+# greater(a,b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
