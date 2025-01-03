0b5df18895d51d28be8289c9b2c6d01af10f99e7
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2025-01-03 15:59:20 +0500

    Add files via upload

---
+++ enumrate_fun.py
@@ -0,0 +1,55 @@
+
+
+# marks=[32,44,55,66,87,98]
+# index=0     
+# for mark in marks:
+#     print(mark)
+   
+#     if(index ==3):
+#         print("Herry awsome")
+#     index+=1
+
+
+# we can do the same work like this 
+# marks=[32,44,55,66,87,98]
+# for index , marks in enumerate(marks):
+#     print(index,marks)
+
+
+
+# fruit=["mango","orange","Banana"]
+# for index,fruit in enumerate(fruit):
+#     print(index,fruit)
+#     if(index==1):
+#         print("cheeku")
+
+
+
+
+# # Q printing the index no and all veriables
+# name="abdul samad jamali"
+# for index,name in enumerate(name,start=1  ):
+#     print(index,name)
+
+
+
+
+
+
+
+
+# import abdul
+# abdul.wellcome()
+
+
+
+
+
+
+
+
+
+
+
+
+
