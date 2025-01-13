9cdf7a8d7f13bc093cffea0046cdf66c7f919a8f
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2025-01-13 12:30:02 +0500

    Add files via upload

---
+++ ExceptionHandling.py
@@ -0,0 +1,33 @@
+# a=input("Enter any number")
+# print("The multiplicative table of ",a ,"is ")
+
+# try:
+#  for i in range (1,11):
+#     print(f"{int(a)} x {i} = {int(a)*i}")
+# except ValueError:
+#  print("Hello word")
+
+
+# try:
+#  num=int(input("Enter eny number"))
+# #  print(num)
+#  a=[1,2,3]
+#  print(a[num])
+# except ValueError:
+#  print("Invalid number ")
+# except IndexError:
+#  print("Rong index ")
+  
+
+# try:
+#  name=int(input("Enter your name "))
+#  a=[1,2,3,4,5,6,7,8,9,0]
+#  print(a[name])
+#  print(name)
+# except ValueError:
+#   print("Invalid input")
+# except IndexError:
+#   print("Invaid index this index is not in our range ")
+# finally:
+#   print("Kuch b hooo mana to print hona ha ")
+  
