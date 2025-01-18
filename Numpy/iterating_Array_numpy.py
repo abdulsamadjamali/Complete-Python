c16ac8d6651ac466e210483a2795cc308d7dc0b6
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2025-01-18 22:06:47 +0500

    Add files via upload

---
+++ Numpy/iterating_Array_numpy.py
@@ -0,0 +1,18 @@
+import numpy as np
+a1=([1,2,3,4,5,6,7,8,9,10,])
+
+a2=([[1,2,3,4],
+     [5,6,7,8]])
+
+a3=([[[1,2,3,4]
+      ,[5,6,7,8]]
+     ,[[9,10,11,12]
+       ,[13,14,15,16]]])
+
+# for i in a2:
+#     print(i)
+# for i in np.nditer(a2):
+#     print(i)
+# (nditer) even 100D array it in to one and then iterate
+for i in np.nditer(a3):
+    print(i)
