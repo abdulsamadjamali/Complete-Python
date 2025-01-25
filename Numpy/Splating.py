a10bdcf2ee77d26012cd70591f363009e72a8a9a
abdulsamadjamali <160044266+abdulsamadjamali@users.noreply.github.com>
2025-01-25 11:30:39 +0500

    Update Splating.py

--- Numpy/Splating.py
+++ Numpy/Splating.py
@@ -3,7 +3,7 @@ import numpy as np
 # means here we break down the array 
 a1=np.arange(1,13).reshape(3,4)
 print(a1)
-print("\n\n\n")
+print("\n\n\n\n")
 print(np.hsplit(a1,2))
 
 print("\n\n\n in actuall it's a horizantal split " )
